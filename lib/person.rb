0c540a1bc41b2c67ff2c0a8bb070d7929b53e3f0
Seth David Gyimah <esec26@gmail.com>
2022-08-29 22:33:09 +0000

    metaprogramming mass assighnement lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-  # your code here
+  
+# instant objects initializes with any number of attributes
+# iterate over each attribute noting each key and it's value
+# for each key, create attr_accessor of the the key, and use send (setter method) to set the value of the key to it's original value
+#   Example: attr_accessor :key,  self.send("#{key}=", value) OR self.key = value
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
